73d8bf2aca7d547e4c738e0cd3bd5c831c0483b7
Ajmal Hossain <37928155+ajmal1971@users.noreply.github.com>
2022-03-23 22:37:14 +0600

    Create XOR.cs

---
+++ XOR.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace HTML_Tag_Tracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Take Two Number:: ");
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+
+            Console.Write("\nBefore XOR a: " + first + " b: " + second);
+
+            first = first ^ second;
+            second = first ^ second;
+            first = first ^ second;
+
+            Console.Write("\nAfter XOR a: " + first + " b: " + second);
+
+            Console.Read();
+        }
+    }
+}
